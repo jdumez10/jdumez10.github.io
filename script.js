9e672f850200bd45aa56be2b3b1b1eab4f9b0e36
Juan Diego Dumez <59177163+jdumez10@users.noreply.github.com>
2023-08-27 01:21:40 -0400

    Add files via upload
    updated files (resume part updated and contact section updated too)

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-const header = document.querySelector("header");
+document.addEventListener("DOMContentLoaded", function() {
+    const header = document.querySelector("header");
+    const menu = document.querySelector("#menu-icon");
+    const navlist = document.querySelector('.navlist');
 
-window.addEventListener("scroll", function(){
-    header.classList.toggle("sticky", this.window.scrollY > 120);
+    window.addEventListener("scroll", function(){
+        header.classList.toggle("sticky", this.window.scrollY > 120);
+    });
 
-})
+    menu.onclick = () => {
+        menu.classList.toggle('bx-x');
+        navlist.classList.toggle('active');
+    };
 
-let menu = document.querySelector("#menu-icon");
-let navlist = document.querySelector('.navlist');
+    // Smooth scroll function
+    function smoothScroll(target, duration) {
+        // ... (rest of the smoothScroll function)
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('active');
-}
+    }
 
-menu.onscroll = () => {
-    menu.remove.toggle('bx-x');
-    navlist.remove.toggle('active');
-}
+    // Smooth scroll when the "Contact Me" button is clicked
+    const contactButton = document.querySelector('.resume-btn');
+    contactButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        smoothScroll('#contact', 1000); // Adjust the duration as needed
+    
+        // Close the mobile menu if open
+        menu.classList.remove('bx-x');
+        navlist.classList.remove('active');
+    });
 
-
-
+    // Open resume PDF
+    document.getElementById("open-resume").addEventListener('click', function() {
+        const pdfUrl = "https://github.com/jdumez10/jdumez10.github.io/blob/main/resume/Resume%20Juan%20Diego%20Dumez.pdf";
+        window.open(pdfUrl, "_blank");
+    });
+});
